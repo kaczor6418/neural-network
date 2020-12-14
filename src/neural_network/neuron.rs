45edcd323c3bf4bf19bc0eb6e0cc81371bcc18ae
kaczor6418 <k.kaczynski97@gmail.com>
2020-12-14 23:15:58 +0100

    refactor(neuron): refactor new method

--- src/neural_network/neuron.rs
+++ src/neural_network/neuron.rs
@@ -1,16 +1,32 @@
+use rand::Rng;
+
 pub struct Neuron {
-    inputs: Vec<f64>,
-    weights: Vec<f64>,
+    weights: Vec<f64>
 }
 
 impl Neuron {
-    pub fn new(weights: Vec<f64>) -> Result<Neuron, String> {
-        let mut inputs = vec![];
-        weights.iter().for_each(|_| inputs.push(0.0));
+    pub fn new(weights_count: &usize, min_weight: &f64, max_weight: &f64) -> Result<Neuron, String> {
+        if Neuron::is_min_weight_less_then_max_weight(&min_weight, &max_weight) == false {
+            return Err(format!("Min weight has to be smaller then max weight.\n Min weight: {}, Max weight: {}", min_weight, max_weight));
+        }
+        let weights = Neuron::generate_random_weights(&weights_count, &min_weight, &max_weight);
         return Ok(Neuron {
-            weights,
-            inputs,
+            weights
         });
+    }
+
+    fn is_min_weight_less_then_max_weight(min_weight: &f64, max_weight: &f64) -> bool {
+        return min_weight < max_weight;
+    }
+
+    fn generate_random_weights(weights_count: &usize, min_weight: &f64, max_weight: &f64) -> Vec<f64> {
+        let mut i = 0;
+        let mut weights: Vec<f64> =  vec![0.0];
+        while &i < weights_count {
+            weights.push(rand::thread_rng().gen_range(min_weight, max_weight));
+            i += 1;
+        }
+        return weights;
     }
 
     pub fn update_weight_value(&mut self, index: usize, value: f64) {
@@ -21,21 +37,9 @@ impl Neuron {
         self.weights = weights;
     }
 
-    pub fn update_input_value(&mut self, index: usize, value: f64) {
-        self.inputs[index] = value;
-    }
-
-    pub fn set_inputs_values(&mut self, inputs: Vec<f64>) {
-        self.inputs = inputs;
-    }
-
-    pub fn calculate_output_value(&self) -> f64 {
-        let mut output = 0.0;
+    pub fn calculate_output_value(&self, inputs: Vec<f64>) -> f64 {
         let mut weights_iterator = self.weights.iter();
-        self.inputs.iter().for_each(|input| {
-            output += input * weights_iterator.next().unwrap()
-        });
-        return output;
+        return inputs.iter().map(|input| input * weights_iterator.next().unwrap()).sum();
     }
 }
 
--- src/neural_network/neuron/neuron_test.rs
+++ src/neural_network/neuron/neuron_test.rs
@@ -2,122 +2,132 @@ mod new {
     use crate::neural_network::neuron::Neuron;
 
     #[test]
-    fn should_create_neuron_if_inputs_and_weights_have_same_size() {
-        let weights = vec![1.0];
-        let neuron = Neuron::new(weights);
+    fn should_create_neuron_if_min_weight_is_smaller_than_max_weight() {
+        let weights_count: usize = 1;
+        let min_weight = 0.0;
+        let max_weight = 1.0;
+        let neuron = Neuron::new(&weights_count, &min_weight, &max_weight);
         assert!(neuron.is_ok());
     }
-}
-
-mod update_weight_value {
-    use crate::neural_network::neuron::Neuron;
 
     #[test]
-    fn should_update_weight_value_if_weight_exists() {
-        let weights = vec![1.0];
-        let mut neuron = Neuron::new(weights).unwrap();
-        let index = 0;
-        let value = 1.1;
-        neuron.update_weight_value(index, value);
-        assert_eq!(neuron.weights[index], value);
-    }
-
-    #[test]
-    #[should_panic]
-    fn should_not_update_weight_value_if_weight_dose_not_exists() {
-        let weights = vec![1.0];
-        let neron_size = weights.len();
-        let mut neuron = Neuron::new(weights).unwrap();
-        neuron.update_weight_value(neron_size, 2.1);
+    fn should_not_create_neuron_if_min_weight_is_bigger_than_max_weight() {
+        let weights_count: usize = 1;
+        let min_weight = 1.0;
+        let max_weight = 0.0;
+        let neuron = Neuron::new(&weights_count, &min_weight, &max_weight);
+        assert!(neuron.is_err());
     }
 }
 
-mod update_input_value {
-    use crate::neural_network::neuron::Neuron;
-
-    #[test]
-    fn should_update_input_value_if_weight_exists() {
-        let weights = vec![1.0];
-        let mut neuron = Neuron::new(weights).unwrap();
-        let index = 0;
-        let value = 1.1;
-        neuron.update_input_value(index, value);
-        assert_eq!(neuron.inputs[index], value);
-    }
-
-    #[test]
-    #[should_panic]
-    fn should_not_update_input_value_if_weight_dose_not_exists() {
-        let weights = vec![1.0];
-        let neron_size = weights.len();
-        let mut neuron = Neuron::new(weights).unwrap();
-        neuron.update_input_value(neron_size, 2.1);
-    }
-}
-
-mod set_inputs_values {
-    use crate::neural_network::neuron::Neuron;
-
-    #[test]
-    fn should_set_inputs_values() {
-        let weights = vec![1.0, 2.0];
-        let inputs = vec![3.0, 4.0];
-        let mut neuron = Neuron::new(weights).unwrap();
-        neuron.set_inputs_values(inputs.clone());
-        assert_eq!(neuron.inputs, inputs);
-    }
-}
-
-mod set_weights_values {
-    use crate::neural_network::neuron::Neuron;
-
-    #[test]
-    fn should_set_inputs_values() {
-        let weights = vec![1.0, 2.0];
-        let new_weights = vec![3.0, 4.0];
-        let mut neuron = Neuron::new(weights).unwrap();
-        neuron.set_weights_values(new_weights.clone());
-        assert_eq!(neuron.weights, new_weights);
-    }
-}
-
-mod calculate_output_value {
-    use crate::neural_network::neuron::Neuron;
-
-    #[test]
-    fn should_calculate_value() {
-        let inputs = vec![1.0, 2.0];
-        let weights = vec![1.0, 2.0];
-        let expected_output_value = inputs[0] * weights[0] + inputs[1] * weights[1];
-        let mut neuron = Neuron::new(weights).unwrap();
-        neuron.set_inputs_values(inputs);
-        let output_value = neuron.calculate_output_value();
-        assert_eq!(output_value, expected_output_value);
-    }
-
-    #[test]
-    fn should_calculate_value_for_changed_weight() {
-        let inputs = vec![1.0, 2.0];
-        let weights = vec![1.0, 2.0];
-        let value = 2.0;
-        let expected_output_value = inputs[0] * value + inputs[1] * weights[1];
-        let mut neuron = Neuron::new(weights).unwrap();
-        neuron.set_inputs_values(inputs);
-        neuron.update_weight_value(0, value);
-        let output_value = neuron.calculate_output_value();
-        assert_eq!(output_value, expected_output_value);
-    }
-
-    #[test]
-    fn should_calculate_value_for_changed_input() {
-        let inputs = vec![1.0, 2.0];
-        let weights = vec![1.0, 2.0];
-        let value = 2.0;
-        let expected_output_value = inputs[0] * value + inputs[1] * weights[1];
-        let mut neuron = Neuron::new(weights).unwrap();
-        neuron.set_inputs_values(inputs);
-        neuron.update_input_value(0, value);
-        let output_value = neuron.calculate_output_value();
-        assert_eq!(output_value, expected_output_value);
-    }
-}
+// mod update_weight_value {
+//     use crate::neural_network::neuron::Neuron;
+//
+//     #[test]
+//     fn should_update_weight_value_if_weight_exists() {
+//         let mut neuron = Neuron::new(&1, &0.0, &1.0).unwrap();
+//         let index = 0;
+//         let value = 1.1;
+//         neuron.update_weight_value(index, value);
+//         assert_eq!(neuron.weights[index], value);
+//     }
+//
+//     #[test]
+//     #[should_panic]
+//     fn should_not_update_weight_value_if_weight_dose_not_exists() {
+//         let weights = vec![1.0];
+//         let neron_size = weights.len();
+//         let mut neuron = Neuron::new(weights).unwrap();
+//         neuron.update_weight_value(neron_size, 2.1);
+//     }
+// }
+//
+// mod update_input_value {
+//     use crate::neural_network::neuron::Neuron;
+//
+//     #[test]
+//     fn should_update_input_value_if_weight_exists() {
+//         let weights = vec![1.0];
+//         let mut neuron = Neuron::new(weights).unwrap();
+//         let index = 0;
+//         let value = 1.1;
+//         neuron.update_input_value(index, value);
+//         assert_eq!(neuron.inputs[index], value);
+//     }
+//
+//     #[test]
+//     #[should_panic]
+//     fn should_not_update_input_value_if_weight_dose_not_exists() {
+//         let weights = vec![1.0];
+//         let neron_size = weights.len();
+//         let mut neuron = Neuron::new(weights).unwrap();
+//         neuron.update_input_value(neron_size, 2.1);
+//     }
+// }
+//
+// mod set_inputs_values {
+//     use crate::neural_network::neuron::Neuron;
+//
+//     #[test]
+//     fn should_set_inputs_values() {
+//         let weights = vec![1.0, 2.0];
+//         let inputs = vec![3.0, 4.0];
+//         let mut neuron = Neuron::new(weights).unwrap();
+//         neuron.set_inputs_values(inputs.clone());
+//         assert_eq!(neuron.inputs, inputs);
+//     }
+// }
+//
+// mod set_weights_values {
+//     use crate::neural_network::neuron::Neuron;
+//
+//     #[test]
+//     fn should_set_inputs_values() {
+//         let weights = vec![1.0, 2.0];
+//         let new_weights = vec![3.0, 4.0];
+//         let mut neuron = Neuron::new(weights).unwrap();
+//         neuron.set_weights_values(new_weights.clone());
+//         assert_eq!(neuron.weights, new_weights);
+//     }
+// }
+//
+// mod calculate_output_value {
+//     use crate::neural_network::neuron::Neuron;
+//
+//     #[test]
+//     fn should_calculate_value() {
+//         let inputs = vec![1.0, 2.0];
+//         let weights = vec![1.0, 2.0];
+//         let expected_output_value = inputs[0] * weights[0] + inputs[1] * weights[1];
+//         let mut neuron = Neuron::new(weights).unwrap();
+//         neuron.set_inputs_values(inputs);
+//         let output_value = neuron.calculate_output_value();
+//         assert_eq!(output_value, expected_output_value);
+//     }
+//
+//     #[test]
+//     fn should_calculate_value_for_changed_weight() {
+//         let inputs = vec![1.0, 2.0];
+//         let weights = vec![1.0, 2.0];
+//         let value = 2.0;
+//         let expected_output_value = inputs[0] * value + inputs[1] * weights[1];
+//         let mut neuron = Neuron::new(weights).unwrap();
+//         neuron.set_inputs_values(inputs);
+//         neuron.update_weight_value(0, value);
+//         let output_value = neuron.calculate_output_value();
+//         assert_eq!(output_value, expected_output_value);
+//     }
+//
+//     #[test]
+//     fn should_calculate_value_for_changed_input() {
+//         let inputs = vec![1.0, 2.0];
+//         let weights = vec![1.0, 2.0];
+//         let value = 2.0;
+//         let expected_output_value = inputs[0] * value + inputs[1] * weights[1];
+//         let mut neuron = Neuron::new(weights).unwrap();
+//         neuron.set_inputs_values(inputs);
+//         neuron.update_input_value(0, value);
+//         let output_value = neuron.calculate_output_value();
+//         assert_eq!(output_value, expected_output_value);
+//     }
+// }
