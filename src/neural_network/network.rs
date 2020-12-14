2d13d75eefa65cd2333989d392c56873e458aaef
kaczor6418 <k.kaczynski97@gmail.com>
2020-12-14 22:27:38 +0100

    wip(config): create config structure

--- src/neural_network/network.rs
+++ src/neural_network/network.rs
@@ -3,15 +3,25 @@ use crate::neural_network::layer::Layer;
 pub mod config;
 
 pub struct Network {
-    layers: Vec<Layer>
+    layers: Vec<Layer>,
+    inputs: Vec<f64>
 }
 
 impl Network {
     pub fn new(layers: Option<Vec<Layer>>) -> Network {
         return Network {
-            layers: layers.unwrap_or(vec![])
+            layers: layers.unwrap_or(vec![]),
+            inputs: vec![]
         };
     }
+
+    // pub fn configure_network(&mut self, config: Config) {
+    //     self.inputs = config.inputs;
+    //     config.layers.iter().for_each(|layer| {
+    //         let neural_layer = Layer::new(None)
+    //     })
+    // }
+
 }
 
 
--- src/neural_network/network/network_test.rs
+++ src/neural_network/network/network_test.rs
@@ -1,21 +1,21 @@
-use crate::neural_network::network::config::Config;
+use crate::neural_network::network::config::{Config, LayerConfig, LayerConfigWithNeuronsCount, BaseLayerConfig, LayerConfigWithNeuronsWeights};
 
 fn valid_config_with_three_layers() -> Config {
     return Config {
         inputs: vec![1.0, 2.0, 3.0],
-        layers_neurons_weights: vec![
-            vec![
-                vec![1.0, 2.0, 3.0],
-                vec![1.0, 2.0, 3.0],
-                vec![1.0, 2.0, 3.0]
-            ],
-            vec![
-                vec![2.0, 3.0, 4.0],
-                vec![2.0, 3.0, 4.0]
-            ],
-            vec![
-                vec![3.0, 4.0, 5.0],
-            ]
+        layers: vec![
+            LayerConfig {
+                layer_config_with_neurons_count: LayerConfigWithNeuronsCount {
+                    base_data: BaseLayerConfig { activation_callback: None },
+                    neurons_count: 12,
+                }
+            },
+            LayerConfig {
+                layer_config_with_neurons_count: LayerConfigWithNeuronsCount {
+                    base_data: BaseLayerConfig { activation_callback: None },
+                    neurons_count: 10,
+                }
+            }
         ],
     };
 }
@@ -23,19 +23,27 @@ fn valid_config_with_three_layers() -> Config {
 fn invalid_config() -> Config {
     return Config {
         inputs: vec![1.0, 2.0, 3.0],
-        layers_neurons_weights: vec![
-            vec![
-                vec![1.0, 2.0, 3.0],
-                vec![1.0, 2.0],
-                vec![1.0]
-            ],
-            vec![
-                vec![2.0, 3.0, 4.0],
-                vec![2.0, 3.0]
-            ],
-            vec![
-                vec![3.0, 4.0, 5.0],
-            ]
+        layers: vec![
+            LayerConfig {
+                layer_config_with_neurons_weights: LayerConfigWithNeuronsWeights {
+                    base_data: BaseLayerConfig { activation_callback: None },
+                    neurons_weights: vec![
+                        vec![1.0],
+                        vec![2.0, 3.0],
+                        vec![3.0, 5.0, 6.0],
+                    ],
+                }
+            },
+            LayerConfig {
+                layer_config_with_neurons_weights: LayerConfigWithNeuronsWeights {
+                    base_data: BaseLayerConfig { activation_callback: None },
+                    neurons_weights: vec![
+                        vec![1.0, 2.0, 3.0],
+                        vec![2.0, 3.0],
+                        vec![3.0],
+                    ],
+                }
+            }
         ],
     };
 }
@@ -58,21 +66,21 @@ mod new {
     }
 }
 
-mod configure_network {
-    use crate::neural_network::network::Network;
-    use crate::neural_network::network::network_test::{valid_config_with_three_layers, invalid_config};
-
-    #[test]
-    fn should_configure_network_for_valid_config() {
-        let network = Network::new(None);
-        network.configure_network(valid_config_with_three_layers());
-        assert_eq!(network.layers, 3)
-    }
-
-    #[test]
-    #[should_panic]
-    fn should_not_configure_network_for_invalid_config() {
-        let network = Network::new(None);
-        network.configure_network(invalid_config());
-    }
-}
+// mod configure_network {
+//     use crate::neural_network::network::Network;
+//     use crate::neural_network::network::network_test::{valid_config_with_three_layers, invalid_config};
+//
+//     #[test]
+//     fn should_configure_network_for_valid_config() {
+//         let network = Network::new(None);
+//         network.configure_network(valid_config_with_three_layers());
+//         assert_eq!(network.layers, 3)
+//     }
+//
+//     #[test]
+//     #[should_panic]
+//     fn should_not_configure_network_for_invalid_config() {
+//         let network = Network::new(None);
+//         network.configure_network(invalid_config());
+//     }
+// }
