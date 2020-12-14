a374a2df0a42aefbe5f4cca09587264e181739a0
kaczor6418 <k.kaczynski97@gmail.com>
2020-12-15 00:03:08 +0100

    refactor(layer): refactor new method

--- src/neural_network/layer.rs
+++ src/neural_network/layer.rs
@@ -6,19 +6,23 @@ pub struct Layer {
 }
 
 impl Layer {
-    pub fn new(neurons: Option<Vec<Neuron>>, activation_callback: Option<fn(value: f64) -> f64>) -> Layer {
+    pub fn new(activation_callback: Option<fn(value: f64) -> f64>) -> Layer {
         return Layer {
-            neurons: neurons.unwrap_or(vec![]),
+            neurons: vec![],
             activation_callback: activation_callback.unwrap_or(|value: f64| value),
         };
     }
 
-    pub fn add_neuron(&mut self, neuron: Neuron) {
-        self.neurons.push(neuron);
+    pub fn add_neurons(&mut self, neurons_count: &usize, inputs_count: &usize, min_weight: &f64, max_weight: &f64) {
+        let mut i = 0;
+        while &i < neurons_count {
+            self.neurons.push(Neuron::new(inputs_count, min_weight, max_weight).unwrap());
+            i += 1;
+        }
     }
 
-    pub fn get_outputs(&self) -> Vec<f64> {
-        return self.neurons.iter().map(|neuron| (self.activation_callback)(neuron.calculate_output_value())).collect();
+    pub fn get_outputs(&self, inputs: Vec<f64>) -> Vec<f64> {
+        return self.neurons.iter().map(|neuron| (self.activation_callback)(neuron.calculate_output_value(&inputs))).collect();
     }
 }
 
--- src/neural_network/layer/layer_test.rs
+++ src/neural_network/layer/layer_test.rs
@@ -4,70 +4,53 @@ mod new {
 
     #[test]
     fn should_create_empty_neural_layer() {
-        let empty_layer = Layer::new(None, None);
+        let empty_layer = Layer::new(None);
         assert_eq!(empty_layer.neurons.len(), 0);
     }
-
-    #[test]
-    fn should_create_neural_layer_with_one_neuron() {
-        let weights = vec![0.0, 1.0, 2.0];
-        let neurons = vec![Neuron::new(weights).unwrap()];
-        let layer = Layer::new(Some(neurons), None);
-        assert_eq!(layer.neurons.len(), 1);
-    }
-
-    #[test]
-    fn should_create_neural_layer_with_two_neuron() {
-        let weights1 = vec![0.0, 1.0, 2.0];
-        let weights2 = vec![0.1, 1.2, 2.3];
-        let neurons = vec![Neuron::new(weights1).unwrap(), Neuron::new(weights2).unwrap()];
-        let layer = Layer::new(Some(neurons), None);
-        assert_eq!(layer.neurons.len(), 2);
-    }
 }
-
-mod add_neuron {
-    use crate::neural_network::layer::Layer;
-    use crate::neural_network::neuron::Neuron;
-
-    #[test]
-    fn should_add_neuron_to_empty_layer() {
-        let weights = vec![0.0, 1.0, 2.0];
-        let mut empty_layer = Layer::new(None, None);
-        empty_layer.add_neuron(Neuron::new(weights).unwrap());
-        assert_eq!(empty_layer.neurons.len(), 1);
-    }
-
-    #[test]
-    fn should_add_neuron_to_layer_with_neurons() {
-        let weights1 = vec![0.0, 1.0, 2.0];
-        let weights2 = vec![0.1, 1.2, 2.3];
-        let neurons = vec![Neuron::new(weights1).unwrap()];
-        let mut layer = Layer::new(Some(neurons), None);
-        layer.add_neuron(Neuron::new(weights2).unwrap());
-        assert_eq!(layer.neurons.len(), 2);
-    }
-}
-
-mod get_outputs {
-    use crate::neural_network::layer::Layer;
-    use crate::neural_network::neuron::Neuron;
-
-    #[test]
-    fn should_return_one_outputs_for_one_neurons() {
-        let weights = vec![0.0, 1.0, 2.0];
-        let neuron = Neuron::new(weights).unwrap();
-        let expected_output =  vec![neuron.calculate_output_value()];
-        let layer = Layer::new(Some(vec![neuron]), None);
-        assert_eq!(layer.get_outputs(), expected_output);
-    }
-
-    #[test]
-    fn should_return_three_outputs_for_three_neurons() {
-        let weights = vec![vec![0.0, 1.0, 2.0], vec![3.0, 4.0, 5.0], vec![6.0, 7.0, 8.0]];
-        let neurons: Vec<Neuron> = weights.iter().map(|weight| Neuron::new(weight.clone()).unwrap()).collect();
-        let expected_output: Vec<f64> =  neurons.iter().map(|neuron| neuron.calculate_output_value()).collect();
-        let layer = Layer::new(Some(neurons), None);
-        assert_eq!(layer.get_outputs(), expected_output);
-    }
-}
+//
+// mod add_neuron {
+//     use crate::neural_network::layer::Layer;
+//     use crate::neural_network::neuron::Neuron;
+//
+//     #[test]
+//     fn should_add_neuron_to_empty_layer() {
+//         let weights = vec![0.0, 1.0, 2.0];
+//         let mut empty_layer = Layer::new(None, None);
+//         empty_layer.add_neuron(Neuron::new(weights).unwrap());
+//         assert_eq!(empty_layer.neurons.len(), 1);
+//     }
+//
+//     #[test]
+//     fn should_add_neuron_to_layer_with_neurons() {
+//         let weights1 = vec![0.0, 1.0, 2.0];
+//         let weights2 = vec![0.1, 1.2, 2.3];
+//         let neurons = vec![Neuron::new(weights1).unwrap()];
+//         let mut layer = Layer::new(Some(neurons), None);
+//         layer.add_neuron(Neuron::new(weights2).unwrap());
+//         assert_eq!(layer.neurons.len(), 2);
+//     }
+// }
+//
+// mod get_outputs {
+//     use crate::neural_network::layer::Layer;
+//     use crate::neural_network::neuron::Neuron;
+//
+//     #[test]
+//     fn should_return_one_outputs_for_one_neurons() {
+//         let weights = vec![0.0, 1.0, 2.0];
+//         let neuron = Neuron::new(weights).unwrap();
+//         let expected_output =  vec![neuron.calculate_output_value()];
+//         let layer = Layer::new(Some(vec![neuron]), None);
+//         assert_eq!(layer.get_outputs(), expected_output);
+//     }
+//
+//     #[test]
+//     fn should_return_three_outputs_for_three_neurons() {
+//         let weights = vec![vec![0.0, 1.0, 2.0], vec![3.0, 4.0, 5.0], vec![6.0, 7.0, 8.0]];
+//         let neurons: Vec<Neuron> = weights.iter().map(|weight| Neuron::new(weight.clone()).unwrap()).collect();
+//         let expected_output: Vec<f64> =  neurons.iter().map(|neuron| neuron.calculate_output_value()).collect();
+//         let layer = Layer::new(Some(neurons), None);
+//         assert_eq!(layer.get_outputs(), expected_output);
+//     }
+// }
