066fa7300577d97f1b64d310a77044d42e52182c
kaczor6418 <k.kaczynski97@gmail.com>
2020-12-15 13:50:34 +0100

    test(network): refactor network tests and create new one for forward propagation

--- src/neural_network/network/network_test.rs
+++ src/neural_network/network/network_test.rs
@@ -19,7 +19,7 @@ fn valid_config_with_three_layers() -> NetworkConfig {
         inputs: vec![1.0, 2.0, 3.0],
         layers: vec![
             LayerConfig {
-                neurons_count: 2,
+                neurons_count: 4,
                 activation_callback: None
             },
             LayerConfig {
@@ -27,7 +27,7 @@ fn valid_config_with_three_layers() -> NetworkConfig {
                 activation_callback: None
             },
             LayerConfig {
-                neurons_count: 4,
+                neurons_count: 5,
                 activation_callback: None
             },
         ]
@@ -48,5 +48,22 @@ mod new {
     fn should_create_network_with_three_layers() {
         let network = Network::new(valid_config_with_three_layers());
         assert_eq!(network.layers.len(), 3)
+    }
+}
+
+mod forward_propagation {
+    use crate::neural_network::network::Network;
+    use crate::neural_network::network::network_test::{valid_config_with_three_layers, valid_config_with_one_layer};
+
+    #[test]
+    fn should_contains_output_with_two_values() {
+        let network = Network::new(valid_config_with_one_layer());
+        assert_eq!(network.forward_propagation().len(), 2);
+    }
+
+    #[test]
+    fn should_contains_output_with_five_values() {
+        let network = Network::new(valid_config_with_three_layers());
+        assert_eq!(network.forward_propagation().len(), 5);
     }
 }
