1ca0a48d0bdab7df8f8ac35ce66f2de1701a05a4
kaczor6418 <k.kaczynski97@gmail.com>
2020-12-16 13:27:13 +0100

    refactor(neuron): move private method to rest private methods

--- src/neural_network/neuron.rs
+++ src/neural_network/neuron.rs
@@ -11,20 +11,6 @@ impl Neuron {
         };
     }
 
-    fn generate_random_weights(
-        weights_count: &usize,
-        min_weight: &f64,
-        max_weight: &f64,
-    ) -> Vec<f64> {
-        let mut i = 0;
-        let mut weights: Vec<f64> = vec![];
-        while &i < weights_count {
-            weights.push(rand::thread_rng().gen_range(min_weight, max_weight));
-            i += 1;
-        }
-        return weights;
-    }
-
     pub fn update_weight(&mut self, index: usize, learning_rate: &f64, loss_value: &f64) {
         self.set_weight(
             index,
@@ -38,6 +24,20 @@ impl Neuron {
             .iter()
             .map(|input| input * weights_iterator.next().unwrap())
             .sum();
+    }
+
+    fn generate_random_weights(
+        weights_count: &usize,
+        min_weight: &f64,
+        max_weight: &f64,
+    ) -> Vec<f64> {
+        let mut i = 0;
+        let mut weights: Vec<f64> = vec![];
+        while &i < weights_count {
+            weights.push(rand::thread_rng().gen_range(min_weight, max_weight));
+            i += 1;
+        }
+        return weights;
     }
 
     fn set_weight(&mut self, index: usize, value: f64) {
