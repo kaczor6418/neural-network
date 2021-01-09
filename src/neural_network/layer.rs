4d4b237c7d917984722033cdec7ae266ec0a48f0
kaczor6418 <k.kaczynski97@gmail.com>
2021-01-09 17:03:00 +0100

    refactor(layer): use new matrix struct in layer implementation

--- src/neural_network/layer.rs
+++ src/neural_network/layer.rs
@@ -1,71 +1,124 @@
+use crate::matrix::matrix::Matrix;
+use crate::neural_network::layer::layer_config::{LayerConfig, WeightsRange};
 use crate::neural_network::neuron::Neuron;
 
+pub mod layer_config;
+
 pub struct Layer {
-    activation_function: fn(value: f64) -> f64,
+    activation_function: fn(value: &f64) -> f64,
     activation_function_derivative: fn(value: &f64) -> f64,
     neurons: Vec<Neuron>,
-    values: Vec<f64>,
+    values: Matrix,
 }
 
 impl Layer {
-    pub fn new(
-        activation_function: &Option<fn(value: f64) -> f64>,
-        activation_function_derivative: &Option<fn(value: &f64) -> f64>,
-    ) -> Layer {
-        return Layer {
-            activation_function: activation_function.unwrap_or(|value: f64| value),
-            activation_function_derivative: activation_function_derivative
+    pub fn new(config: LayerConfig, inputs_count: usize) -> Layer {
+        let mut layer = Layer {
+            activation_function: config
+                .activation_function
+                .unwrap_or(|value: &f64| value.clone()),
+            activation_function_derivative: config
+                .activation_function_derivative
                 .unwrap_or(|_value: &f64| 1.0),
             neurons: vec![],
-            values: vec![],
+            values: Matrix::new_zeros_matrix(1, inputs_count),
         };
+        layer.add_neurons(config.neurons_count, inputs_count, config.weights_range);
+        return layer;
+    }
+
+    pub fn calculate_values(&mut self, inputs: &Matrix) {
+        self.values.set_values(
+            self.neurons
+                .iter()
+                .map(|neuron| (self.activation_function)(&neuron.calculate_output_value(&inputs)))
+                .collect(),
+        );
+    }
+
+    pub fn correct_neurons_weight(
+        &mut self,
+        learning_rate: &f64,
+        prev_layer_values: &Matrix,
+        next_layer_delta: &Matrix,
+        next_layer_weights: &Matrix,
+    ) {
+        let new_weights = self.get_neurons_weights()
+            - learning_rate
+                * self.calculate_wights_delta(
+                    prev_layer_values,
+                    next_layer_delta,
+                    next_layer_weights,
+                );
+        for row_index in 0..new_weights.rows_count() {
+            self.neurons[row_index]
+                .get_weights()
+                .set_values(new_weights.get_values().clone())
+        }
+    }
+
+    pub fn get_values(&self) -> &Matrix {
+        return &self.values;
     }
 
     pub fn size(&self) -> usize {
         return self.neurons.len();
     }
 
-    pub fn add_neurons(
+    fn add_neurons(
         &mut self,
-        neurons_count: &usize,
-        inputs_count: &usize,
-        min_weight: &f64,
-        max_weight: &f64,
+        neurons_count: usize,
+        inputs_count: usize,
+        weights_range: WeightsRange,
     ) {
         let mut i = 0;
-        while &i < neurons_count {
-            self.neurons
-                .push(Neuron::new(inputs_count, min_weight, max_weight));
+        while i < neurons_count {
+            self.neurons.push(Neuron::new(
+                &inputs_count,
+                &weights_range.min_weight,
+                &weights_range.max_weight,
+            ));
             i += 1;
         }
     }
 
-    pub fn calculate_outputs(&mut self, inputs: &Vec<f64>) -> &Vec<f64> {
-        self.values = self
-            .neurons
-            .iter()
-            .map(|neuron| (self.activation_function)(neuron.calculate_output_value(inputs)))
-            .collect();
-        return &self.values;
+    fn calculate_derivative_values(&self) -> Matrix {
+        return Matrix::new(
+            self.values.columns_count(),
+            self.values
+                .get_values()
+                .iter()
+                .map(|value| (self.activation_function_derivative)(value))
+                .collect(),
+        );
     }
 
-    pub fn calculate_weight_delta(&self, expected_values: &Vec<f64>) -> Vec<f64> {
-        let mut expected_values_iter = expected_values.iter();
-        return self
-            .values
-            .iter()
-            .map(|value| {
-                (expected_values_iter.next().unwrap() - value)
-                    * (self.activation_function_derivative)(value)
-            })
-            .collect();
+    fn calculate_layer_delta(
+        &self,
+        next_layer_delta: &Matrix,
+        next_layer_weights: &Matrix,
+    ) -> Matrix {
+        return next_layer_delta * next_layer_weights * &self.calculate_derivative_values();
     }
 
-    pub fn correct_neurons_weight(&mut self, delta_values: &Vec<f64>, learning_rate: &f64) {
-        let mut delta_values_iterator = delta_values.iter();
-        for (index, neuron) in self.neurons.iter_mut().enumerate() {
-            neuron.update_weight(index, learning_rate, delta_values_iterator.next().unwrap());
-        }
+    fn calculate_wights_delta(
+        &self,
+        prev_layer_values: &Matrix,
+        next_layer_delta: &Matrix,
+        next_layer_weights: &Matrix,
+    ) -> Matrix {
+        return self.calculate_layer_delta(next_layer_delta, next_layer_weights)
+            * prev_layer_values;
+    }
+
+    fn get_neurons_weights(&self) -> Matrix {
+        return Matrix::new(
+            self.neurons[0].get_weights().get_values().len(),
+            self.neurons
+                .iter()
+                .map(|neuron| neuron.get_weights().get_values())
+                .collect(),
+        );
     }
 }
 
